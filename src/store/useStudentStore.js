14574abe64425f881ec81fbef5b11b035b75526c
Suresh6667 <suresh24612@gmail.com>
2024-12-24 16:42:09 +0530

    Update frontend to connect with the backend

--- src/store/useStudentStore.js
+++ src/store/useStudentStore.js
@@ -3,7 +3,7 @@
 import { create } from 'zustand';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/students';
+const API_URL = process.env.REACT_APP_BACKEND_URL;
 
 const useStudentStore = create((set) => ({
   students: [],
